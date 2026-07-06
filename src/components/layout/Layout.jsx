30869d071af9dded56ba122ea2fdf9fbc5a318e1
larythonia <larythonia@gmail.com>
2026-07-06 13:04:37 +0100

    .

--- src/components/layout/Layout.jsx
+++ src/components/layout/Layout.jsx
@@ -1,13 +1,12 @@
 import { Outlet } from "react-router-dom";
 import Navbar from "./Navbar";
 
-console.log("LAYOUT RENDERING");
  function Layout() {
   return (
     <>
       <Navbar />
 
-      <main className=" min-w-[80vw] py-25 px-30">
+      <main className="py-25 px-30">
         <Outlet />
       </main>
     </>
